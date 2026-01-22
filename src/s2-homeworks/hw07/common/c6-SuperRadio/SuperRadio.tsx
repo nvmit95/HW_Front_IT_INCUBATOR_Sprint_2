e6d1cb415ab84feac6a64e91237f143e288e87dd
Nikita Mitsin <nvmit95@gmail.com>
2026-01-22 17:56:20 +0300

    HW_7_Sprint_3

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -35,7 +35,9 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ...restProps
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // делают студенты
+     if(onChangeOption){
+       onChangeOption(Number(e.target.value))
+     }
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
@@ -48,6 +50,9 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
+                      name={name}
+                      value={o.id}
+                      checked={value === o.id}
                       // name, checked, value делают студенты
 
                       onChange={onChangeCallback}
