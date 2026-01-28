a66f848a41e61047afd7af422bf130bf51a3f15a
Nikita Mitsin <nvmit95@gmail.com>
2026-01-28 22:13:34 +0300

    HW_8_Sprint_3

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,24 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+  switch (action.type) {
+    case 'sort': { // by name
+      const newState = [...state];
+      if (action.payload === 'up') {
+        return newState.sort((a, b) => a.name.localeCompare(b.name))
+      } else {
+        return newState.sort((a, b) => b.name.localeCompare(a.name))
+      }
     }
+    case 'check': {
+
+      return state.filter(user => user.age >= action.payload)
+    }
+    default:
+      return state
+  }
 }
